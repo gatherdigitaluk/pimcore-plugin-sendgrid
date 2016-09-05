5a7d4fee2c845d0de669d713e7e50462e3ff8bec
Paul Clegg <paul@gatherdigital.co.uk>
2016-09-05 11:53:48 +0100

    still throw the Exceptions in Debug mode for pimcore

--- lib/Sendgrid/Client.php
+++ lib/Sendgrid/Client.php
@@ -10,9 +10,15 @@ class Client extends \SendGrid
     public function send(\SendGrid\Email $email)
     {
         try {
+
             $response = parent::send($email);
+
         } catch (\SendGrid\Exception $e) {
             \Logger::error($e->getMessage());
+
+            if (PIMCORE_DEBUG) {
+                throw $e;
+            }
         }
     }
 
